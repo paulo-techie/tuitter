8e2b998402057f0c129c3d7c8be438a2fd6403f2
Paul Omondi <paulo.techie@gmail.com>
2020-08-27 02:48:29 +0300

    add who to follow feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,16 +11,6 @@ class SessionsController < ApplicationController
     redirect_to user_tueets_path(user)
   end
 
-  # def auth
-  #   username = params[:username].to_s
-  #   user = User.find_by_username(username)
-
-  #   redirect_to 'login_path' and return if !user
-  #   session[:user_id] = user.id
-    
-  #   redirect_to user, notice: 'You are signed in!'
-  # end
-
   def destroy
     session[:user_id] = nil   
     redirect_to root_url, notice: 'Logged out!'   
